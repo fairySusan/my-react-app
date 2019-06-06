846edbb46bbbee7aca35716224c24102be5d0e95
fairySusan <792209917@qq.com>
2019-06-06 15:43:56 +0800

    新增server.js文件

---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+app.use(bodyParser.json());
+// 跨域设置
+app.all('*', function (req, res, next) {
+    res.append("Access-Control-Allow-Credentials", true)
+    res.append("Access-Control-Allow-Origin", "http://localhost:3000")
+    res.append("Access-Control-Allow-Headers", ["X-Requested-With","Content-Type","Authorization"])
+    res.append("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS")
+    res.append("X-Powered-By", ' 3.2.1')
+    res.append("Content-Type", "application/json;charset=utf-8")
+    next()
+})
+/**
+ * 登录
+ */
+app.post('/login',(req, res) => {
+    const result = {
+        username: req.body.username,
+        role: 'manager',
+        token: '10011'
+    }
+    res.send(result);
+})
+var server = app.listen(3001,function(){
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log("应用实例，访问地址为 http://%s:%s", host, port);
+})
