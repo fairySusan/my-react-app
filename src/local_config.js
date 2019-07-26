816bd73ba4144f9ba460adfa9e9ece24349c2c8d
fairySusan <792209917@qq.com>
2019-07-26 15:03:57 +0800

    我也不知道修改了些啥

--- src/local_config.js
+++ src/local_config.js
@@ -1,2 +1,6 @@
-const baseUrl = 'http://127.0.0.1:3000';
+const isProd = process.env.NODE_ENV === 'production';
+let baseUrl = 'http://127.0.0.1:3000';
+if (isProd) {
+    baseUrl = 'http://127.0.0.1:3000';
+}
 export default baseUrl;
